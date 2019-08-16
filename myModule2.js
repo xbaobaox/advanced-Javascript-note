463ca2fe1db178382b94b6671f8bd4403e01b306
xbaobaox <xbaobaox@sina.com>
2019-08-16 22:56:59 +0800

    1

--- myModule2.js
+++ myModule2.js
@@ -1,4 +1,4 @@
-function myModule() {
+(function (window) {
     //私有数据
     var msg = 'someTHING'
     //操作数据的函数
@@ -8,10 +8,9 @@ function myModule() {
     function fn2() {
         console.log('fn2()' + msg.toLowerCase())
     }
-    // return fn1
     //向外暴露对象(给外部使用的方法)
-    return {
-        fn1:fn1,
-        fn2:fn2
+    window.myModule2 = {
+        fn1: fn1,
+        fn2: fn2
     }
-}
+})(window) //将实参传入形参 代码压缩的时候形参会变成w 局部变量的window也会变成w,为了防止出错
